f9143aab39a702869e6a7ca9b28ce12e02fc7481
David Gomesh <davidgomesh2016@gmail.com>
2024-01-15 21:52:14 -0300

    Add fold method to Tuples

--- src/main/fp/Tuples.ts
+++ src/main/fp/Tuples.ts
@@ -3,6 +3,10 @@ export class Tuple2<A, B> {
     _1: A = this.first
     _2: B = this.second
     constructor(public first: A, public second: B) { }
+
+    fold <C> (f: (_1: A, _2: B) => C): C {
+        return f(this._1, this._2)
+    }
 }
 
 export function tuple <A, B> (first: A, second: B): Tuple2<A, B> {
@@ -14,4 +18,8 @@ export class Tuple3<A, B, C> {
     _2: B = this.second
     _3: C = this.third
     constructor(public first: A, public second: B, public third: C) { }
+
+    fold <D> (f: (_1: A, _2: B, _3: C) => D): D {
+        return f(this._1, this._2, this._3)
+    }
 }
