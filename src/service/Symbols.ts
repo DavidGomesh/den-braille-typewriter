c116b1e1ca74d32d648c631f65f004227549503f
David Gomesh <davidgomesh2016@gmail.com>
2024-03-21 09:34:52 -0300

    Remove unused files from service

--- src/service/Symbols.ts
+++
@@ -1,8 +0,0 @@
-import { Cell } from "./Cell"
-import { Empty } from "./Identifiers"
-
-export const Space: Cell = Empty
-export const SPC: Cell = Space
-export function isSpace(cell: Cell): boolean {
-    return cell == Space
-}
--- src/service/converters/Converter.ts
+++
@@ -1,6 +0,0 @@
-import { List } from "immutable"
-import { Cell } from "../Cell.ts"
-
-export default interface Converter {
-    convert(cells: List<Cell>): string
-}
