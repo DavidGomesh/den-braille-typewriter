156f428c3fc0fe9b04f57b079b88cb0975d68f6a
David Gomesh <davidgomesh2016@gmail.com>
2024-03-22 10:50:49 -0300

    Create routes to Home and Lessons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './views/Home.tsx'
-
-import "./vendors/bootstrap/css/bootstrap.min.css"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import "./styles/index.css"
+import "./vendors/bootstrap/css/bootstrap.min.css"
+
+import Home from './views/Home.tsx'
+import Lessons from './views/Lessons.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Home />
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' Component={Home} />
+                <Route path='/lessons' Component={Lessons} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 )
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
 import '../styles/views/Home.css'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (<>
         <main className='container d-flex justify-content-center align-items-center'>
             <div className='text-center'>
-                <h1 style={{fontSize: '5 rem'}}>Máquina Den Braille</h1>
+                <h1 style={{fontSize: '5rem'}}>Máquina Den Braille</h1>
                 <div className='d-flex flex-column align-items-center'>
                     <button className='btn btn-outline-primary btn-lg fw-bold mb-2' style={{width: '300px'}}>Modo livre</button>
-                    <button className='btn btn-outline-primary btn-lg fw-bold mb-2' style={{width: '300px'}}>Modo lições</button>
+                    <Link to='/lessons'><button className='btn btn-outline-primary btn-lg fw-bold mb-2' style={{width: '300px'}}>Modo lições</button></Link>
                     <button className='btn btn-outline-primary btn-lg fw-bold mb-2' style={{width: '300px'}}>Opções</button>
                     <button className='btn btn-outline-primary btn-lg fw-bold mb-2' style={{width: '300px'}}>Sobre</button>
                 </div>
