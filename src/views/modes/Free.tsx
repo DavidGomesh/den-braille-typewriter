2097b9fb5aebec1975b74adf6e517f43ebd4c0a7
David Gomesh <davidgomesh2016@gmail.com>
2024-10-15 12:51:28 -0300

    Update mode views

--- src/views/modes/Free.tsx
+++ src/views/modes/Free.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Typewriter from '../../components/Typewriter.tsx'
 
 import '../../styles/views/modes/Free.css'
+import CellAudioPlayer from '../../components/CellAudioPlayer.tsx'
 
 export default function Free() {
-    console.log('fds')
     return (<>
         <main>
-            <Typewriter />
+            <CellAudioPlayer>
+                <Typewriter />
+            </CellAudioPlayer>
         </main>
     </>)
 }
